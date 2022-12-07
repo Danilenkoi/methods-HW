5981143c708fda123ad7bf57d56f51a73c58d28d
iladanilenko <Danilenkoi@inbox.ru>
2022-12-07 16:56:33 +0300

    Репозиторий для дз методы

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,73 @@
 public class Main {
+
+    //Exercise 1
+
+    public static int printLeapYears(int howYear) {
+        int years = howYear;
+        if (years % 4 == 0 || years % 100 == 0 || years % 400 == 0) {
+            System.out.println(years + " год является високосным");
+        } else {
+            System.out.println(years + " год не является високосным");
+        }
+        return years;
+    }
+
+    //Exercise 2
+
+    public static int getClientOS(int yearDevice, int osDevice) {
+        int clientDeviceYear = yearDevice;
+        int clientOS = osDevice;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите lite версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите lite версию приложения для Android по ссылке");
+        }
+        return clientOS;
+    }
+
+    //Exercise 3
+
+    public static int deliveryDay(int distance, int one, int two, int three){
+        int deliveryDistance = distance;
+        int day = one;
+        int twoDay = two;
+        int threeDay = three;
+        if (deliveryDistance > 1 && deliveryDistance <= 20){
+            System.out.println("Потребуется дней: " + day);
+        } else if(deliveryDistance > 20 && deliveryDistance <= 60){
+            System.out.println("Потребуется дней: " + twoDay);
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100){
+            System.out.println("Потребуется дней: " + threeDay);
+        }
+        return deliveryDistance;
+    }
+
+
+
     public static void main(String[] args) {
+
+        //exercise 1
+        int numbersYear = 2022;
+        printLeapYears(numbersYear);
+
+        //exercise 2
+        int yearDevice = 2020;
+        int osDevice = 0;
+        // IOS - 0; Android - 1
+        getClientOS(yearDevice, osDevice);
+
+        //exercise 3
+        int distance = 95;
+        int one = 1;
+        int two = one + 1;
+        int three = two + 1;
+        deliveryDay(distance, one, two, three);
     }
 }
+
+
+
